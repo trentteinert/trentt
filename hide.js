2c02072d4753137159873823364a80c482d99dec
trentteinert <102979377+trentteinert@users.noreply.github.com>
2022-05-14 21:08:41 -0400

    init

--- hide.js
+++ hide.js
@@ -1,9 +1,9 @@
 function hide() {
   let image = document.getElementById("img1");
     image.src ="/components/img1.jpg"
-    image.style ="display:none; margin-left:25px; margin-bottom:0px;"
-    document.getElementById("btnID1")
+    image.style ="display:none; margin-left:35px; margin-bottom:0px;"
+    document.getElementById("contentbtnID1")
       .style.display = "inline-block";
-    document.getElementById("btnID2")
+    document.getElementById("contentbtnID2")
       .style.display = "none";
 }
--- show.js
+++ show.js
@@ -1,9 +1,9 @@
 function show() {
   let image = document.getElementById("img1");
     image.src ="/components/img1.jpg"
-    image.style ="height:40%; width:40%; margin-left:25px; margin-bottom:0px;"
-    document.getElementById("btnID1")
+    image.style ="height:40%; width:40%; margin-left:35px; margin-bottom:0px; margin-top:0px;"
+    document.getElementById("contentbtnID1")
       .style.display = "none";
-    document.getElementById("btnID2")
+    document.getElementById("contentbtnID2")
       .style.display = "inline-block";
 }
