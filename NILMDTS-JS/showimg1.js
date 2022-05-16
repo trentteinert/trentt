02a9c6329c10c60c7cbb0c8fd31f0dc8b750481e
trentteinert <102979377+trentteinert@users.noreply.github.com>
2022-05-16 12:19:29 -0400

    init

---
+++ NILMDTS-JS/showimg1.js
@@ -0,0 +1,11 @@
+function showimg1() {
+  var x = document.getElementById("img2");
+    if (x.style.display === "none") {x.style.display = "inline-flex";} 
+    else {x.style.display = "none";}
+  var x = document.getElementById("contentbtnID3");
+    if (x.style.display === "block") {x.style.display = "none";} 
+    else {x.style.display = "block";}
+  var x = document.getElementById("contentbtnID4");
+    if (x.style.display === "none") {x.style.display = "block";} 
+    else {x.style.display = "none";}
+}
