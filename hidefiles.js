7118112a50f7ef43344586c41c9caa687d942e8b
trentteinert <102979377+trentteinert@users.noreply.github.com>
2022-06-20 11:15:36 -0400

    init

---
+++ hidefiles.js
@@ -0,0 +1,8 @@
+function hidefiles(){
+  var x = document.getElementById("filebuttons");
+  if (x.style.display === "flex") {x.style.display = "none";} 
+    else {x.style.display = "none";}
+    var x = document.getElementById("filebuttons");
+  if (x.style.display === "none") {x.style.display = "none";} 
+    else {x.style.display = "none";}
+}
